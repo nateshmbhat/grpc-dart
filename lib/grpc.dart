3c187245b453bb20fa262d45b33813864596409e
nateshmbhat <nateshmbhat1@gmail.com>
2020-03-15 18:42:15 +0530

    exposed http client implmentation

--- lib/grpc.dart
+++ lib/grpc.dart
@@ -27,6 +27,7 @@ export 'src/client/client.dart' show Client;
 export 'src/client/common.dart' show Response, ResponseStream, ResponseFuture;
 export 'src/client/connection.dart' show ConnectionState ,ClientConnection ;
 export 'src/client/http2_channel.dart' show ClientChannel;
+export 'src/client/http2_connection.dart' show Http2ClientConnection;
 export 'src/client/method.dart' show ClientMethod;
 export 'src/client/options.dart'
     show
